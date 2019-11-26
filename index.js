52237a11634c431059e61e9661c74b83d56feeec
Manoj Bhat <bhatanm@gmail.com>
2019-11-26 15:34:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const socket = require("socket.io");
+const express = require("express");
+
+const app = express();
+
+const server = app.listen(3001, () => console.log("Listining to port 3001"));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const io = socket(server);
+
+const users = [];
+
+io.on("connection", socket => {
+  io.emit("AVILABLE_USERS", users);
+  console.log(`${socket.id} connected`);
+  socket.on("SET_USER", user => {
+    users.push({ [socket.id]: user });
+    io.emit("AVILABLE_USERS", users);
+    console.log(users);
+  });
+  socket.on("chat", data => {
+    socket.broadcast.emit("MESSAGE_RECIEVED", data);
+  });
+  socket.on("typing", data => {
+    socket.broadcast.emit("typing", data);
+  });
+  socket.on("disconnect", () => {
+    console.log(socket.id + " disconnected");
+    let index = -1;
+    for (let i = 0; i < users.length; i++) {
+      if (Object.keys(users[i])[0] === socket.id) {
+        index = i;
+        break;
+      }
+    }
+    users.splice(index, 1);
+  });
+});
